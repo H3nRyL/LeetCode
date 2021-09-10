8aaeda1862e3f3ac48da707f0088307e0b69abd5
H3nRyL <67525181+H3nRyL@users.noreply.github.com>
2021-09-10 14:06:35 -0700

    Time: 489 ms (16.00%), Space: 116.2 MB (6.26%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -4,24 +4,20 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         
         for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
-            if (i == 0 || nums[i - 1] != nums[i])
-                twoSum(nums, i, result);
-        }
+            if (i == 0 || nums[i - 1] != nums[i]) {
+                Set<Integer> seen = new HashSet<>();
+                for (int j = i + 1; j < nums.length; j++) {
+                    int complement = -nums[i] - nums[j];
+                    if (seen.contains(complement)) {
+                        result.add(Arrays.asList(nums[i], nums[j], complement));
+                        while (j + 1< nums.length && nums[j] == nums[j + 1])
+                            j++;
+                    }
+                    seen.add(nums[j]);
+                }
+            }
+        } 
         
         return result;
-    }
-    
-    private void twoSum(int[] nums, int i, List<List<Integer>> result) {
-        Set<Integer> seen = new HashSet<>();
-        
-        for (int j = i + 1; j < nums.length; j++) {
-            int complement = -nums[i] - nums[j];
-            if (seen.contains(complement)) {
-                result.add(Arrays.asList(nums[i], nums[j], complement));
-                while (j + 1 < nums.length && nums[j] == nums[j + 1])
-                    j++;
-            }
-            seen.add(nums[j]);
-        }
     }
 }
