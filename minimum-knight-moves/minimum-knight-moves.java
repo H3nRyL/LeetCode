77fcf56ffb4b0e3ce658bc95810d6c8369d94218
H3nRyL <67525181+H3nRyL@users.noreply.github.com>
2021-09-18 11:52:16 -0700

    Time: 201 ms (60.98%), Space: 45.8 MB (74.71%) - LeetHub

---
+++ minimum-knight-moves/minimum-knight-moves.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minKnightMoves(int x, int y) {
+        int[][] offsets = {{1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}};
+        boolean[][] visited = new boolean[605][605];
+        Deque<int[]> queue = new LinkedList<>();
+        queue.addLast(new int[]{0, 0});
+        int steps = 0;
+        
+        while (queue.size() > 0) {
+            int currSize = queue.size();
+            for (int i = 0; i < currSize; i++) {
+                int[] curr = queue.removeFirst();
+                if (curr[0] == x && curr[1] == y)
+                    return steps;
+                
+                for (int[] offset : offsets) {
+                    int[] next = new int[]{curr[0] + offset[0], curr[1] + offset[1]};
+                    if (!visited[next[0] + 302][next[1] + 302]) {
+                        visited[next[0] + 302][next[1] + 302] = true;
+                        queue.addLast(next);
+                    }
+                }
+            }
+            steps++;            
+        }
+        
+        return steps;
+    }
+}
