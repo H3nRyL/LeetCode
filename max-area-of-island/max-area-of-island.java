609b89d2c23f1c1de95ecb928570409a66af86cf
H3nRyL <67525181+H3nRyL@users.noreply.github.com>
2021-09-21 13:57:33 -0700

    Time: 2 ms (99.60%), Space: 40 MB (41.85%) - LeetHub

--- max-area-of-island/max-area-of-island.java
+++ max-area-of-island/max-area-of-island.java
@@ -7,16 +7,16 @@ class Solution {
         
         for (int r = 0; r < grid.length; r++)
             for (int c = 0; c < grid[0].length; c++)
-                result = Math.max(result, area(r, c));
+                result = Math.max(result, dfs(r, c));
         
         return result;
     }
     
-    private int area(int r, int c) {
+    private int dfs(int r, int c) {
         if (r < 0 || r >= grid.length || c < 0 || c >= grid[0].length || grid[r][c] == 0)
             return 0;
         
         grid[r][c] = 0;
-        return 1 + area(r + 1, c) + area(r - 1, c) + area(r, c + 1) + area(r, c - 1);
+        return 1 + dfs(r + 1, c) + dfs(r - 1, c) + dfs(r, c + 1) + dfs(r, c - 1);
     }
 }
