0e694acc0a5727e4e983ef42ba213c852821626a
H3nRyL <67525181+H3nRyL@users.noreply.github.com>
2021-09-26 16:36:29 -0700

    Time: 2 ms (32.75%), Space: 37.2 MB (82.63%) - LeetHub

--- decode-ways/decode-ways.java
+++ decode-ways/decode-ways.java
@@ -1,27 +1,28 @@
 class Solution {
-    private Map<Integer, Integer> map = new HashMap<>();
+    private Map<Integer, Integer> map;
     
     public int numDecodings(String s) {
-        return recursiveWithMemo(0, s); 
+        map = new HashMap<>();    
+        return recursiveWithMemo(0, s);
     }
     
     private int recursiveWithMemo(int index, String str) {
-        if (map.containsKey(index)) 
+        if (map.containsKey(index))
             return map.get(index);
         
         if (index == str.length())
             return 1;
         
         if (str.charAt(index) == '0')
-           return 0;
-            
+            return 0;
+        
         if (index == str.length() - 1)
             return 1;
-            
+        
         int result = recursiveWithMemo(index + 1, str);
         if (Integer.parseInt(str.substring(index, index + 2)) <= 26)
             result += recursiveWithMemo(index + 2, str);
-            
+        
         map.put(index, result);
         return result;
     }
