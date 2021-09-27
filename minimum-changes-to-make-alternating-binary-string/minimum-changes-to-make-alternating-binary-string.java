ee29213bc000ada5dbd7195a7f738b3c74624551
H3nRyL <67525181+H3nRyL@users.noreply.github.com>
2021-09-27 11:16:49 -0700

    Time: 3 ms (69.17%), Space: 38.9 MB (71.29%) - LeetHub

--- minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -1,13 +1,11 @@
 class Solution {
     public int minOperations(String s) {
         int result = 0;
-        int n = s.length();
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < s.length(); i++) 
             if (s.charAt(i) - '0' != i % 2)
                 result++;
-        }
         
-        return Math.min(result, n - result);
+        return Math.min(result, s.length() - result);
     }
 }
