35e1aba10b0f6e300b622601de525af24860c3d5
Haoying Li <67525181+H3nRyL@users.noreply.github.com>
2021-10-14 15:43:21 -0700

    Time: 3 ms (61.99%), Space: 56.1 MB (5.24%) - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -1,27 +1,29 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int result = 0;
+        if (grid == null || grid.length == 0)
+            return 0;
         
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == '1') {
+        int result = 0;
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                if (grid[r][c] == '1') {
                     result++;
-                    markIsland(grid, i, j);
-                }    
+                    dfs(grid, r, c);
+                }
             }
         }
         
         return result;
     }
     
-    private void markIsland(char[][] grid, int r, int c) {
-        if (r < 0 || c < 0 || r >= grid.length || c >= grid[r].length || grid[r][c] == '0')
+    private void dfs(char[][] grid, int r, int c) {
+        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == '0')
             return;
         
         grid[r][c] = '0';
-        markIsland(grid, r - 1, c);
-        markIsland(grid, r + 1, c);
-        markIsland(grid, r, c - 1);
-        markIsland(grid, r, c + 1);
+        dfs(grid, r + 1, c);
+        dfs(grid, r - 1, c);
+        dfs(grid, r, c + 1);
+        dfs(grid, r, c - 1);
     }
 }
