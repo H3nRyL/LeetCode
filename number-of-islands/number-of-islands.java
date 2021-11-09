f298d042838d61e5d7ec1cb061f3379f57dc2e9f
Haoying Li <67525181+H3nRyL@users.noreply.github.com>
2021-11-08 21:56:39 -0800

    Time: 2 ms (100.00%), Space: 47.6 MB (52.29%) - LeetHub

--- number-of-islands/number-of-islands.java
+++ number-of-islands/number-of-islands.java
@@ -4,11 +4,11 @@ class Solution {
             return 0;
         
         int result = 0;
-        for (int r = 0; r < grid.length; r++) {
-            for (int c = 0; c < grid[0].length; c++) {
-                if (grid[r][c] == '1') {
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[0].length; col++) {
+                if (grid[row][col] == '1') {
                     result++;
-                    dfs(grid, r, c);
+                    dfs(grid, row, col);
                 }
             }
         }
@@ -16,14 +16,14 @@ class Solution {
         return result;
     }
     
-    private void dfs(char[][] grid, int r, int c) {
-        if (r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] == '0')
+    private void dfs(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0')
             return;
         
-        grid[r][c] = '0';
-        dfs(grid, r + 1, c);
-        dfs(grid, r - 1, c);
-        dfs(grid, r, c + 1);
-        dfs(grid, r, c - 1);
+        grid[row][col] = '0';
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
     }
 }
