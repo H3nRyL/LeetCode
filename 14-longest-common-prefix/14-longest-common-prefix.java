29f91d4be8fdb9a394abd29725860a3504e76b1d
Haoying Li <67525181+H3nRyL@users.noreply.github.com>
2022-02-06 18:59:27 -0500

    Time: 2 ms (49.98%), Space: 41.7 MB (16.83%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < strs[0].length(); i++) {
             char c = strs[0].charAt(i);
             for (int j = 1; j < strs.length; j++) {
-                if (strs[j].length() <= i || strs[j].charAt(i) != c)
+                if (strs[j].length() == i || strs[j].charAt(i) != c)
                     return sb.toString();
             }
             sb.append(c);
