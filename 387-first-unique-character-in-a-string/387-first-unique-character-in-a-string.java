f7ab957f5de5a2bdd8e18af1d973b21f04dd73bf
Haoying Li <67525181+H3nRyL@users.noreply.github.com>
2022-02-13 18:06:01 -0500

    Time: 41 ms (36.81%), Space: 54.5 MB (6.50%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -5,8 +5,8 @@ class Solution {
             map.put(c, map.getOrDefault(c, 0) + 1);
         
         for (int i = 0; i < s.length(); i++)
-                if (map.get(s.charAt(i)) == 1)
-                    return i;
+            if (map.get(s.charAt(i)) == 1)
+                return i;
         
         return -1;
     }
