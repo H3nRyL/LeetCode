0857350168de15717be25b5d84718e4b423ca095
Haoying Li <67525181+H3nRyL@users.noreply.github.com>
2022-03-27 22:29:25 -0700

    Time: 54 ms (15.81%), Space: 55.8 MB (6.18%) - LeetHub

--- 929-unique-email-addresses/929-unique-email-addresses.java
+++ 929-unique-email-addresses/929-unique-email-addresses.java
@@ -1,21 +1,15 @@
 class Solution {
     public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < emails.length; i++) {
             String localName = emails[i].split("@")[0];
             String domainName = emails[i].split("@")[1];
             
             String valid = localName.split("\\+")[0].replaceAll("\\.", "");
             emails[i] = valid + "@" + domainName;
+            set.add(emails[i]);
         }
         
-        Set<String> set = new HashSet<>();
-        int result = 0;
-        for (String s : emails) {
-            if (!set.contains(s))
-                result++;
-            set.add(s);
-        }
-        
-        return result;
+        return set.size();
     }
 }
